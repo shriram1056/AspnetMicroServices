fa9b8afd472102352edc06bf8ef1816f35223018
shriram saravanan <shriram.saravanan@bluwave-ai.com>
2023-06-27 05:32:27 -0700

    StackExchangeRedis DI

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddStackExchangeRedisCache(options => options.Configuration = builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
