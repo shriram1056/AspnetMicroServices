af85124f75d2ca0b3377b50d76628882346551f7
shriram saravanan <shriram.saravanan@bluwave-ai.com>
2023-07-02 19:03:17 -0300

    Discount Services

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Discount.API.Extensions;
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 using Npgsql;
@@ -14,10 +16,15 @@ builder.Services.AddGrpc();
 builder.Services.AddTransient<IDbConnection>((sp) => new NpgsqlConnection(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString")));
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
 var app = builder.Build();
 
+app.MigrateDatabase<Program>();
 // Configure the GreeterService as a gRPC service endpoint.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
